70c459bbc99a873879e4c166c2ce9af35be4acd3
Phung Nguyen <longphung.nn@gmail.com>
2022-10-26 15:35:29 +1100

    ✨ feat: show all fields

--- app/serializers.py
+++ app/serializers.py
@@ -5,28 +5,28 @@ from app.models import Question, QuestionAnswer, SessionProgress, Session, Sessi
 class QuestionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Question
-        fields = ['description', 'level', 'type', 'feedback']
+        fields = '__all__'
 
 
 class QuestionAnswerSerializer(serializers.ModelSerializer):
     class Meta:
         model = QuestionAnswer
-        fields = ['question_id', 'description', 'is_correct']
+        fields = '__all__'
 
 
 class SessionProgressSerializer(serializers.ModelSerializer):
     class Meta:
         model = SessionProgress
-        fields = ['student_id', 'session_id', 'progress', 'attended']
+        fields = '__all__'
 
 
 class SessionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Session
-        fields = ['id', 'start_time', 'end_time', 'instructor_id']
+        fields = '__all__'
 
 
 class SessionQuestionsSerializer(serializers.ModelSerializer):
     class Meta:
         model = SessionsQuestions
-        fields = ['session_id', 'question_id']
+        fields = '__all__'
