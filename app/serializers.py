6969d66db349eb23963c61c485d81ad0b1a87f65
Phung Nguyen <longphung.nn@gmail.com>
2022-10-26 15:31:36 +1100

    ✨ feat: change to model serializers

--- app/serializers.py
+++ app/serializers.py
@@ -2,31 +2,31 @@ from rest_framework import serializers
 from app.models import Question, QuestionAnswer, SessionProgress, Session, SessionsQuestions
 
 
-class QuestionSerializer(serializers.HyperlinkedModelSerializer):
+class QuestionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Question
         fields = ['description', 'level', 'type', 'feedback']
 
 
-class QuestionAnswerSerializer(serializers.HyperlinkedModelSerializer):
+class QuestionAnswerSerializer(serializers.ModelSerializer):
     class Meta:
         model = QuestionAnswer
         fields = ['question_id', 'description', 'is_correct']
 
 
-class SessionProgressSerializer(serializers.HyperlinkedModelSerializer):
+class SessionProgressSerializer(serializers.ModelSerializer):
     class Meta:
         model = SessionProgress
         fields = ['student_id', 'session_id', 'progress', 'attended']
 
 
-class SessionSerializer(serializers.HyperlinkedModelSerializer):
+class SessionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Session
-        fields = ['start_time', 'end_time', 'instructor_id']
+        fields = ['id', 'start_time', 'end_time', 'instructor_id']
 
 
-class SessionQuestionsSerializer(serializers.HyperlinkedModelSerializer):
+class SessionQuestionsSerializer(serializers.ModelSerializer):
     class Meta:
         model = SessionsQuestions
         fields = ['session_id', 'question_id']
